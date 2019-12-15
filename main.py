7232d72694d0567ac6a791368f799ea6d0b4b20e
zheng <zhengwangsea@gmail.com>
2019-12-14 21:23:12 -0800

    d

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy2")
 
 
 @APP.route('/auth', methods=['POST'])
